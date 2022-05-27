dc1705b1aefe5203e92aff91b98e6ed93ab9054f
Attaaa <hatta.eka.p@gmail.com>
2022-05-27 08:05:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quasar Biolerplate
+
+My personal **Quasar V1** boilerplate code with custom VSCose setup. Comes with attrsorter, sass, and other initial settings to make best development experience with Quasar.
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="q-app">
+    <error-404></error-404>
+    <router-view> </router-view>
+  </div>
+</template>
+
+<script>
+import Error404 from "./pages/Error404.vue";
+export default {
+  components: { Error404 },
+  name: "App",
+};
+</script>
